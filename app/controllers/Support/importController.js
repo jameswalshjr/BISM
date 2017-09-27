8763c7b5769147bf2ec5ebb973d39470a3c4f457
James Walsh Jr <walshjrjames@gmail.com>
2017-09-26 21:40:22 -0400

    Added error handling for service not available Added form and form management functionality

--- app/controllers/Support/importController.js
+++ app/controllers/Support/importController.js
@@ -1,7 +1,7 @@
 ﻿angular.module('app.controllers')
-    .controller('importController', ['$scope', '$routeParams', '$http', 'EnvConfig',  function ($scope, $routeParams, $http, EnvConfig) {
+    .controller('importController', ['$scope', '$routeParams', '$http', '$cookies', 'EnvConfig', function ($scope, $routeParams, $http, $cookies, EnvConfig) {
         this.bootMessage = 'Inside Import Controller / View';
-        
+
         if (window.File && window.FileReader && window.FileList && window.Blob) {
 
             var formdata = new FormData();
@@ -25,10 +25,29 @@
                     .then(function (response) {
                         var result = response.data;
                         console.log(response);
+
+                    })
+                    .catch(function (error) {
+                        if (error.status === -1) {
+                            $scope.usageResponse = 'The service is currently unavailable.'
+                        }
                     });
             };
 
-            $scope.resetUsage = function () {
+            $scope.resetUsage = function (form) {
+                var usageInput = $('#fileUsage')
+                usageInput.replaceWith(usageInput.val('').clone(true));
+                form.$setPristine();
+                form.$dirty = false;
+                form.$invalid = true;
+                form.$valid = false;
+                form.formdata = {};
+                $scope.usageResponse = null;
+                $files = null;
+                $scope.UsageUpload = null;
+            };
+
+            $scope.resetUsageRate = function () {
 
             };
 
@@ -36,6 +55,19 @@
             alert(' The File APIs are not fully supported in this browser.');
         }
     }])
+    .directive('validFile', function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, el, attrs, ngModel) {
+                el.bind('change', function () {
+                    scope.$apply(function () {
+                        ngModel.$setViewValue(el.val());
+                        ngModel.$render();
+                    })
+                })
+            }
+        }
+    })
     .directive('ngFiles', ['$parse', function ($parse) {
 
         function fn_link(scope, element, attrs) {
